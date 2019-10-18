dedf854d344bdc2eea1a066ddf9006cf4cf74f1f
jacobortiz <jacortiz@csumb.edu>
2019-10-18 19:05:17 +0000

    updated app.js

--- lab6/app.js
+++ lab6/app.js
@@ -17,6 +17,6 @@ app.get("/venus", function(req, res) {
 });
 
 //server listeners
-app.listen("8081", "127.0.0.1", function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Express Server is running...");
 })
